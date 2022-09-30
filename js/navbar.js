53755c52b8a77d0f7a7d7960b18d79f108cab844
akash <akashfulari007@gmail.com>
2022-09-30 08:40:47 +0530

    dummy file deleted

---
+++ js/navbar.js
@@ -0,0 +1,18 @@
+function saveBig() {
+  document.getElementById("saveBig").style.visibility = "visible";
+}
+function hideBig() {
+  document.getElementById("saveBig").style.visibility = "hidden";
+}
+function saveBig1() {
+  document.getElementById("saveBig1").style.visibility = "visible";
+}
+function hideBig1() {
+  document.getElementById("saveBig1").style.visibility = "hidden";
+}
+
+
+
+
+
+// 
